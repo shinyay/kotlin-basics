ce0b77960137f60551522511d63dec76cb380329
shinyay <shinya.com@gmail.com>
2017-12-22 17:51:11 +0900

    Modify annotation for Mapping
    I changed from RequestMapping to GetMapping

--- 04-springboot/src/main/kotlin/com/shinyay/basics/springboot/controller/MainController.kt
+++ 04-springboot/src/main/kotlin/com/shinyay/basics/springboot/controller/MainController.kt
@@ -2,13 +2,14 @@ package com.shinyay.basics.springboot.controller
 
 import org.springframework.stereotype.Controller
 import org.springframework.ui.Model
-import org.springframework.web.bind.annotation.RequestMapping
+import org.springframework.web.bind.annotation.GetMapping
+import com.shinyay.basics.springboot.model.fizzBuzzLambda
 
 @Controller
 class MainController {
-    @RequestMapping
+    @GetMapping("/")
     fun index(model: Model): String {
-        val fizzbuzz = com.shinyay.basics.springboot.model.fizzBuzzLambda(50)
+        val fizzbuzz = fizzBuzzLambda(50)
         model.addAttribute("message", fizzbuzz)
         return "index"
     }
