9136c73f4398a5cb5f48b6b3b882603dbdddddcf
shinyay <shinya.com@gmail.com>
2017-12-22 13:45:48 +0900

    Add SpiringBoot Controller Class

--- 04-springboot/src/main/kotlin/com/shinyay/basics/springboot/controller/MainController.kt
+++ 04-springboot/src/main/kotlin/com/shinyay/basics/springboot/controller/MainController.kt
@@ -1,4 +1,15 @@
 package com.shinyay.basics.springboot.controller
 
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.RequestMapping
+
+@Controller
 class MainController {
+    @RequestMapping
+    fun index(model: Model): String {
+        val fizzbuzz = com.shinyay.basics.springboot.model.fizzBuzzLambda(50)
+        model.addAttribute("message", fizzbuzz)
+        return "index"
+    }
 }
